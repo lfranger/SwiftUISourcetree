5a78f80e744946fb9254f0bd9dd78eccd2794f8b
Felipe Garrido <lfranger2099@gmail.com>
2022-05-08 15:05:30 -0400

    Initial commit

---
+++ SwiftUISourcetree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISourcetree
+//
+//  Created by Consultant on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISourcetree/SwiftUISourcetreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISourcetreeApp.swift
+//  SwiftUISourcetree
+//
+//  Created by Consultant on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISourcetreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
