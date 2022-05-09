5aed1703e224942077df5b24bc2178782382fdb9
Felipe Garrido <lfranger2099@gmail.com>
2022-05-08 22:36:30 -0400

    Add 'ContentA' view to the project

---
+++ SwiftUISourcetree/ContentA.swift
@@ -0,0 +1,15 @@
+//
+//  ContentA.swift
+//  SwiftUISourcetree
+//
+//  Created by Consultant on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentA: View {
+    var body: some View {
+        Text("Hello, ContentA!")
+            .padding()
+    }
+}
